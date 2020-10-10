03af7b913deeacf33c2355e6b4818fc9eb4eb21d
poe <piotr.a.ptak@icloud.com>
2020-10-10 14:31:01 +0200

    add recognition for celebrities

--- created/main.go
+++ created/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/aws/aws-lambda-go/events"
 
@@ -14,6 +13,11 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
+type Celebrity struct {
+	Name 	string
+	ID 		string
+}
+
 // CreatedHandler ...
 func CreatedHandler(ctx context.Context, event events.S3Event) (bool, error) {
 	svc := rekognition.New(session.New())
@@ -21,49 +25,29 @@ func CreatedHandler(ctx context.Context, event events.S3Event) (bool, error) {
 	for _, record := range event.Records {
 		log.Info().Msgf("%v\n %v", record.S3.Bucket.Name, record.S3.Object.Key)
 
-		input := &rekognition.DetectLabelsInput{
+		celebIn := &rekognition.RecognizeCelebritiesInput{
 			Image: &rekognition.Image{
 				S3Object: &rekognition.S3Object{
 					Bucket: aws.String(record.S3.Bucket.Name),
 					Name:   aws.String(record.S3.Object.Key),
 				},
 			},
-			MaxLabels:     aws.Int64(12),
-			MinConfidence: aws.Float64(70.000000),
 		}
 
-		result, err := svc.DetectLabels(input)
+		celebRes, err := svc.RecognizeCelebrities(celebIn)
 		if err != nil {
-			if aerr, ok := err.(awserr.Error); ok {
-				switch aerr.Code() {
-				case rekognition.ErrCodeInvalidS3ObjectException:
-					fmt.Println(rekognition.ErrCodeInvalidS3ObjectException, aerr.Error())
-				case rekognition.ErrCodeInvalidParameterException:
-					fmt.Println(rekognition.ErrCodeInvalidParameterException, aerr.Error())
-				case rekognition.ErrCodeImageTooLargeException:
-					fmt.Println(rekognition.ErrCodeImageTooLargeException, aerr.Error())
-				case rekognition.ErrCodeAccessDeniedException:
-					fmt.Println(rekognition.ErrCodeAccessDeniedException, aerr.Error())
-				case rekognition.ErrCodeInternalServerError:
-					fmt.Println(rekognition.ErrCodeInternalServerError, aerr.Error())
-				case rekognition.ErrCodeThrottlingException:
-					fmt.Println(rekognition.ErrCodeThrottlingException, aerr.Error())
-				case rekognition.ErrCodeProvisionedThroughputExceededException:
-					fmt.Println(rekognition.ErrCodeProvisionedThroughputExceededException, aerr.Error())
-				case rekognition.ErrCodeInvalidImageFormatException:
-					fmt.Println(rekognition.ErrCodeInvalidImageFormatException, aerr.Error())
-				default:
-					fmt.Println(aerr.Error())
-				}
-			} else {
-				// Print the error, cast err to awserr.Error to get the Code and
-				// Message from an error.
-				fmt.Println(err.Error())
-			}
-			return false, nil
+			handleRekognitionError(err)
 		}
 
-		log.Info().Msgf("%v", result)
+		for _, celeb := range celebRes.CelebrityFaces {
+			log.Info().Msgf("%s found", celeb.Name)
+		}
+
+		nbNoCelebs := len(celebRes.UnrecognizedFaces)
+		log.Info().Msgf("%v unrecognized people", nbNoCelebs)
+		// var  Celebrity
+		// if err = json.Unmarshal(celebRes, )
+		// log.Info().Msgf("%v", celebRes)
 	}
 
 	return true, nil
@@ -71,4 +55,31 @@ func CreatedHandler(ctx context.Context, event events.S3Event) (bool, error) {
 
 func main() {
 	lambda.Start(CreatedHandler)
+}
+
+func handleRekognitionError(err error) {
+	if aerr, ok := err.(awserr.Error); ok {
+		switch aerr.Code() {
+		case rekognition.ErrCodeInvalidS3ObjectException:
+			log.Error().Err(err).Msgf(rekognition.ErrCodeInvalidS3ObjectException, aerr.Error())
+		case rekognition.ErrCodeInvalidParameterException:
+			log.Error().Err(err).Msgf(rekognition.ErrCodeInvalidParameterException, aerr.Error())
+		case rekognition.ErrCodeImageTooLargeException:
+			log.Error().Err(err).Msgf(rekognition.ErrCodeImageTooLargeException, aerr.Error())
+		case rekognition.ErrCodeAccessDeniedException:
+			log.Error().Err(err).Msgf(rekognition.ErrCodeAccessDeniedException, aerr.Error())
+		case rekognition.ErrCodeInternalServerError:
+			log.Error().Err(err).Msgf(rekognition.ErrCodeInternalServerError, aerr.Error())
+		case rekognition.ErrCodeThrottlingException:
+			log.Error().Err(err).Msgf(rekognition.ErrCodeThrottlingException, aerr.Error())
+		case rekognition.ErrCodeProvisionedThroughputExceededException:
+			log.Error().Err(err).Msgf(rekognition.ErrCodeProvisionedThroughputExceededException, aerr.Error())
+		case rekognition.ErrCodeInvalidImageFormatException:
+			log.Error().Err(err).Msgf(rekognition.ErrCodeInvalidImageFormatException, aerr.Error())
+		default:
+			log.Error().Err(err).Msgf(aerr.Error())
+		}
+	} else {
+		log.Error().Err(err).Msgf(err.Error())
+	}
 }
