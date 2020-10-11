abad59f11fb54502e9caa58fffc9f9a2c7930a98
poe <piotr.a.ptak@icloud.com>
2020-10-11 19:41:13 +0200

    make batch send parallel

--- scripts/batch_send_directory.sh
+++ scripts/batch_send_directory.sh
@@ -3,14 +3,16 @@
 API_ENDPOINT=$1
 DIRECTORY=$2
 
-for file in "$DIRECTORY"/*
-do
-    file_name=$(basename $file)
+function send {
+    file_name=$(basename $1)
     extension="${file_name##*.}"
     encoded=$(base64 -i $file)
     JSON="{\"imageBase64\": \"$encoded\", \"fileName\": \"$file_name\", \"extension\": \"$extension\"}"
 
     echo "Uploading file $file_name to $API_ENDPOINT"
     curl -XPOST -d "$JSON" -H "Content-type: application/json" -v $API_ENDPOINT
-done
+}  
 
+for file in "$DIRECTORY"/*; do
+    send $file &
+done
